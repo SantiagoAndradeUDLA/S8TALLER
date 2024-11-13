0436414d83d1acdbaa61a8cfda3af2fcac7744b8
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2024-11-12 21:40:39 -0500

    add  main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+Una fábrica de componentes electrónicos necesita optimizar su producción para cumplir con la demanda de cinco productos distintos.
+Cada producto tiene un tiempo de fabricación específico y requiere una cantidad fija de recursos. El equipo de ingeniería ha 
+identificado que la línea de producción tiene limitaciones de tiempo y recursos que deben ser tomadas en cuenta.
+
+Leer README.md 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
